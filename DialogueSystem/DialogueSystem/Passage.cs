322642aa3401d1682d1570291eb7be27b1cc5b30
ltg7412 <ltg7412@g.rit.edu>
2025-11-05 16:07:40 -0500

    first commit

---
+++ DialogueSystem/DialogueSystem/Passage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DialogueSystem
+{
+    internal class Passage
+    {
+        string text;
+        Option[] options;
+    }
+}
---
+++ DialogueSystem/DialogueSystem/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DialogueSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
