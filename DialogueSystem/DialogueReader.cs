b3f9c82d3fb6e24c6647fb035f43cee86f2f964f
Lev Gluzdov (RIT Student) <ltg7412@rit.edu>
2025-11-07 15:38:03 -0500

    wip reading json

---
+++ DialogueSystem/DialogueReader.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace DialogueSystem
+{
+    internal class DialogueReader
+    {
+        Dialogue dialogue;
+        internal DialogueReader(string filename)
+        {
+            string filePath = @$"Dialogue\{filename}";
+
+            try
+            {
+                string jsonString = File.ReadAllText(filePath);
+                JsonSerializer.Deserialize<>(jsonString);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine($"Error reading Json File '{filePath}': {e.Message}");
+            }
+        }
+    }
+}
